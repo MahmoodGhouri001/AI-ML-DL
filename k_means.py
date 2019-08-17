6882245f01bde77575ef4ead61bf32e584009948
jsprasad80-gmail <36761717+jsprasad80-gmail@users.noreply.github.com>
2019-08-17 12:09:05 +0530

    Create k_means.py

---
+++ k_means.py
@@ -0,0 +1,81 @@
+#import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset=pd.read_csv('./Mall Customers2.csv')
+
+X = dataset.iloc[:, [3, 4]].values
+
+
+dataset.describe()
+
+from sklearn.cluster import KMeans, MiniBatchKMeans
+
+WCSS_array=np.array([])
+for K in range(1,20):
+    kmeans=KMeans(n_clusters = K)
+    kmeans.fit(X)
+    Centroids=kmeans.predict(X)
+    WCSS_array=np.append(WCSS_array,kmeans.inertia_)
+    
+    
+K_array=np.arange(1,20,1)
+plt.plot(K_array,WCSS_array)
+plt.xlabel('Number of Clusters')
+plt.ylabel('within-cluster sums of squares (WCSS)')
+plt.title('Elbow method to determine optimum number of clusters')
+plt.show()
+
+final_K = 100
+
+import datetime
+a = datetime.datetime.now()
+kmeans=KMeans(n_clusters=final_K)
+kmeans.fit(X)
+print(datetime.datetime.now() - a)
+
+cluster_val = kmeans.predict(X)
+cluster_val = cluster_val.reshape(cluster_val.shape[0],1)
+X1 = pd.DataFrame(np.concatenate((X, cluster_val), axis=1), columns = ['Annual Income (k$)', 'Spending Score', 'cluster'])
+
+
+
+color=['red','blue','green','cyan','magenta']
+labels=['cluster1','cluster2','cluster3','cluster4','cluster5']
+for k in range(final_K):
+    plt.scatter(X1.loc[X1['cluster'] == k]['Annual Income (k$)'],X1.loc[X1['cluster'] == k]['Spending Score'],s=100,c=color[k],label=labels[k])
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=500,c='yellow',label='Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
+print(kmeans.inertia_)
+
+a = datetime.datetime.now()
+mbk = MiniBatchKMeans(n_clusters=final_K, batch_size=1000)
+mbk.fit(X)
+print(datetime.datetime.now() - a)
+
+cluster_val = mbk.predict(X)
+cluster_val = cluster_val.reshape(cluster_val.shape[0],1)
+X1 = pd.DataFrame(np.concatenate((X, cluster_val), axis=1), columns = ['x', 'y', 'cluster'])
+
+
+
+color=['red','blue','green','cyan','magenta']
+labels=['cluster1','cluster2','cluster3','cluster4','cluster5']
+for k in range(final_K):
+    plt.scatter(X1.loc[X1['cluster'] == k]['x'],X1.loc[X1['cluster'] == k]['y'],s=100,c=color[k],label=labels[k])
+plt.scatter(mbk.cluster_centers_[:,0],mbk.cluster_centers_[:,1],s=500,c='yellow',label='Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
+print(mbk.inertia_)
+
